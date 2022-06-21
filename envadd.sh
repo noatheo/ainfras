9c253b5397512a7ec4dd4c212894658a7c6f07a0
Nawras <99247994+noatheo@users.noreply.github.com>
2022-06-21 10:31:14 +0300

    Update envadd.sh

--- envadd.sh
+++ envadd.sh
@@ -1,4 +1,4 @@
-gcloud secrets list | awk '{ print $1 }'|sed "1d" |sed  's/^/gcloud secrets versions access 1 --secret=/' | sed '1 i #! /bin/bash' > env.sh
+gcloud secrets list | awk '{ print $1 }'|sed "1d" |sed  's/^/gcloud secrets versions access 1 --secret=/' |sed '0~1 a\echo\' file| sed '1 i #! /bin/bash' > env.sh
 bash env.sh > secrets.txt
 sed -i s/$/\"/ secrets.txt
 sed -i s/^/\"/ secrets.txt
